188ba2e15268664963255f934e35aeb6342ded08
GClunies <greg.clunies@gmail.com>
2020-12-12 12:09:36 -0500

    Grant additional privileges to transformer role

--- snowflake/3_grant_create-schema_privileges_to_loader_and_transformer.sql
+++ snowflake/3_grant_create-schema_privileges_to_loader_and_transformer.sql
@@ -9,3 +9,9 @@
 -- Grant privileges to loader and transformer roles
 grant all on database raw to role loader;
 grant create schema on database analytics to role transformer;
+
+-- From step 6 in dbt guide (https://discourse.getdbt.com/t/the-exact-privileges-we-grant-outside-of-dbt/439):
+--     "Since transformer is creating all the relations within the analytics database,
+--      that role will have all privileges on these relation automatically."
+-- But I found that I had to explicity grant all privileges to transformer role to get dbt-cloud working.
+grant all on database analytics to role transformer;
